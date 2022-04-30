3998ae836f1333e82fb69730c85506383fde3df0
andre-savedra <andresavedracruz@gmail.com>
2022-04-30 00:28:57 -0300

    initial

---
+++ src-gen/debug/objects/application.d
@@ -0,0 +1,95 @@
+C:/Users/andre/XDK-Workspace/FMS/src-gen/debug/objects/application.o: \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/application.c \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ ../Platform/Essentials/include/BCDS_Retcode.h \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ ../Platform/Sensors/include/BCDS_Environmental.h \
+ ../Platform/Sensors/include/BCDS_LightSensor.h \
+ ../Common/legacy/include/XdkSensorHandle.h \
+ ../Common/include/Sensor/XDK_SensorHandle.h \
+ ../Platform/Sensors/include/BCDS_Accelerometer.h \
+ ../Platform/Sensors/include/BCDS_Gyroscope.h \
+ ../Platform/Sensors/include/BCDS_Magnetometer.h \
+ ../Platform/SensorToolbox/include/BCDS_Orientation.h \
+ ../Platform/SensorToolbox/include/BCDS_Rotation.h \
+ ../Platform/SensorToolbox/include/BCDS_Gravity.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h \
+ ../Platform/SensorToolbox/include/BCDS_AbsoluteHumidity.h \
+ ../Platform/SensorToolbox/include/BCDS_StepCounter.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedGyro.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedMag.h \
+ ../Platform/SensorToolbox/include/BCDS_FingerPrint.h \
+ ../Platform/SensorToolbox/include/BCDS_LinearAcceleration.h \
+ ../Platform/SensorToolbox/include/BCDS_Gestures.h \
+ C:\XDK-Workbench\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/ConnectivityMQTTQubitroBroker.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorEnvironment.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorLight.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorGyroscope.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorAccelerometer.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaExceptions.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/application.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/applicationTypes.h
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+../Platform/Essentials/include/BCDS_Retcode.h:
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+../Platform/Sensors/include/BCDS_Environmental.h:
+
+../Platform/Sensors/include/BCDS_LightSensor.h:
+
+../Common/legacy/include/XdkSensorHandle.h:
+
+../Common/include/Sensor/XDK_SensorHandle.h:
+
+../Platform/Sensors/include/BCDS_Accelerometer.h:
+
+../Platform/Sensors/include/BCDS_Gyroscope.h:
+
+../Platform/Sensors/include/BCDS_Magnetometer.h:
+
+../Platform/SensorToolbox/include/BCDS_Orientation.h:
+
+../Platform/SensorToolbox/include/BCDS_Rotation.h:
+
+../Platform/SensorToolbox/include/BCDS_Gravity.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h:
+
+../Platform/SensorToolbox/include/BCDS_AbsoluteHumidity.h:
+
+../Platform/SensorToolbox/include/BCDS_StepCounter.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedGyro.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedMag.h:
+
+../Platform/SensorToolbox/include/BCDS_FingerPrint.h:
+
+../Platform/SensorToolbox/include/BCDS_LinearAcceleration.h:
+
+../Platform/SensorToolbox/include/BCDS_Gestures.h:
+
+C:\XDK-Workbench\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/ConnectivityMQTTQubitroBroker.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorEnvironment.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorLight.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorGyroscope.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorAccelerometer.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaExceptions.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/application.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/applicationTypes.h:
---
+++ src-gen/debug/objects/main.d
@@ -0,0 +1,98 @@
+C:/Users/andre/XDK-Workspace/FMS/src-gen/debug/objects/main.o: \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/main.c \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ ../Libraries/FreeRTOS/3rd-party/include/FreeRTOS.h \
+ ../Common/config/AmazonFreeRTOS/FreeRTOS/FreeRTOSConfig.h \
+ ../Platform/Essentials/include/BCDS_Assert.h \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ ../Platform/Essentials/include/BCDS_Retcode.h \
+ ../Common/config/BCDS_TaskConfig.h \
+ ../Libraries/FreeRTOS/3rd-party/include/private/projdefs.h \
+ ../Libraries/FreeRTOS/3rd-party/include/private/portable.h \
+ ../Libraries/FreeRTOS/3rd-party/include/private/deprecated_definitions.h \
+ ../Libraries/FreeRTOS/3rd-party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ ../Libraries/FreeRTOS/3rd-party/include/private/mpu_wrappers.h \
+ ../Platform/Essentials/include/BCDS_Retcode.h \
+ ../Platform/Utils/include/BCDS_CmdProcessor.h \
+ ../Platform/Utils/include/BCDS_Utils.h \
+ ../Common/config/Utils/BCDS_UtilsConfig.h \
+ C:\XDK-Workbench\XDK\SDK/xdk110/Common/include/XdkSystemStartup.h \
+ ../Libraries/FreeRTOS/3rd-party/include/timers.h \
+ ../Libraries/FreeRTOS/3rd-party/include/task.h \
+ ../Libraries/FreeRTOS/3rd-party/include/private/list.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/application.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/applicationTypes.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorEnvironment.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorLight.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorGyroscope.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorAccelerometer.h \
+ C:\XDK-Workbench\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/ConnectivityMQTTQubitroBroker.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/ConnectivityWLANWifi.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaTime.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaEvents.h \
+ C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaExceptions.h
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+../Libraries/FreeRTOS/3rd-party/include/FreeRTOS.h:
+
+../Common/config/AmazonFreeRTOS/FreeRTOS/FreeRTOSConfig.h:
+
+../Platform/Essentials/include/BCDS_Assert.h:
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+../Platform/Essentials/include/BCDS_Retcode.h:
+
+../Common/config/BCDS_TaskConfig.h:
+
+../Libraries/FreeRTOS/3rd-party/include/private/projdefs.h:
+
+../Libraries/FreeRTOS/3rd-party/include/private/portable.h:
+
+../Libraries/FreeRTOS/3rd-party/include/private/deprecated_definitions.h:
+
+../Libraries/FreeRTOS/3rd-party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+
+../Libraries/FreeRTOS/3rd-party/include/private/mpu_wrappers.h:
+
+../Platform/Essentials/include/BCDS_Retcode.h:
+
+../Platform/Utils/include/BCDS_CmdProcessor.h:
+
+../Platform/Utils/include/BCDS_Utils.h:
+
+../Common/config/Utils/BCDS_UtilsConfig.h:
+
+C:\XDK-Workbench\XDK\SDK/xdk110/Common/include/XdkSystemStartup.h:
+
+../Libraries/FreeRTOS/3rd-party/include/timers.h:
+
+../Libraries/FreeRTOS/3rd-party/include/task.h:
+
+../Libraries/FreeRTOS/3rd-party/include/private/list.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/application.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/applicationTypes.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorEnvironment.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorLight.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorGyroscope.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/SensorAccelerometer.h:
+
+C:\XDK-Workbench\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/ConnectivityMQTTQubitroBroker.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/ConnectivityWLANWifi.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaTime.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaEvents.h:
+
+C:/Users/andre/XDK-Workspace/FMS/src-gen/base/MitaExceptions.h:
